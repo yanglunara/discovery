378d0f2c9d3413c1054b89bc4143c74438260c99
yanghao89 <1491286207@qq.com>
2024-04-12 10:24:09 +0800

    服务发现

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
-module github.com/yunbaifan/discovery
+module github.com/yanglunara/discovery
 
 go 1.20
+
+require google.golang.org/grpc v1.63.2
 
 replace github.com/coreos/bbolt v1.3.9 => go.etcd.io/bbolt v1.3.9
 
@@ -38,7 +40,6 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/genproto v0.0.0-20200513103714-09dca8ec2884 // indirect
-	google.golang.org/grpc v1.33.1 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
---
+++ naming/etcd/register_test.go
@@ -0,0 +1,94 @@
+package etcd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/yanglunara/discovery/register"
+	"go.etcd.io/etcd/clientv3"
+	"google.golang.org/grpc"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestHeartBeat(t *testing.T) {
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: time.Second, DialOptions: []grpc.DialOption{grpc.WithBlock()},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		_ = client.Close()
+	}()
+
+	ctx := context.Background()
+	id, _ := os.Hostname()
+	s := &register.ServiceInstance{
+		ID:      id,
+		Name:    "helloworld",
+		Version: "v1.0",
+	}
+	opts := &Options{
+		Ctx:       ctx,
+		MaxRetry:  5,
+		Ttl:       2 * time.Second,
+		Namespace: "microservices",
+	}
+	go func() {
+		r := NewRegistry(client, opts)
+		w, err1 := r.Watch(ctx, s.Name)
+		if err1 != nil {
+			return
+		}
+		defer func() {
+			_ = w.Stop()
+		}()
+		for {
+			res, err2 := w.Next()
+			if err2 != nil {
+				return
+			}
+			t.Logf("watch: %d", len(res))
+			for _, r := range res {
+				t.Logf("next: %+v", r)
+			}
+		}
+	}()
+	time.Sleep(time.Second)
+
+	// new a server
+	r := NewRegistry(client, opts)
+
+	key := fmt.Sprintf("%s/%s/%s", r.Namespace, s.Name, s.ID)
+	value, _ := json.Marshal(s)
+	r.lease = clientv3.NewLease(r.client)
+	leaseID, err := r.withKV(ctx, key, string(value))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// wait for lease expired
+	time.Sleep(3 * time.Second)
+
+	res, err := r.GetService(ctx, s.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(res) != 0 {
+		t.Errorf("not expected empty")
+	}
+
+	go r.heartBeat(ctx, leaseID, key, string(value))
+
+	time.Sleep(time.Second * 30)
+	res, err = r.GetService(ctx, s.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(res) == 0 {
+		t.Errorf("reconnect failed")
+	}
+}
