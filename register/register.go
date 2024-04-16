9970dab0999d90f66dd09aad26ed252fc1be3e64
yanghao89 <1491286207@qq.com>
2024-04-17 02:11:37 +0800

    update

--- register/register.go
+++ register/register.go
@@ -6,57 +6,62 @@ import (
 	consulApi "github.com/hashicorp/consul/api"
 )
 
+// ServiceInstance 服务实例
 type ServiceInstance struct {
-	// ID is the unique instance ID as registered.
-	ID string `json:"id"`
-	// Name is the service name as registered.
-	Name string `json:"name"`
-	// Version is the version of the compiled.
-	Version string `json:"version"`
-	// Metadata is the kv pair metadata associated with the service instance.
-	Metadata  map[string]string `json:"metadata"`
-	Endpoints []string          `json:"endpoints"`
+	ID        string            `json:"id"`        // 注册的唯一实例ID
+	Name      string            `json:"name"`      // 注册的服务名称
+	Version   string            `json:"version"`   // 编译的版本
+	Metadata  map[string]string `json:"metadata"`  // 与服务实例关联的键值对元数据
+	Endpoints []string          `json:"endpoints"` // 端点
 }
 
+// Registrar 注册器接口
 type Registrar interface {
-	// Register the registration.
+	// 注册服务
 	Register(ctx context.Context, service *ServiceInstance) error
-	// Deregister the registration.
+	// 注销服务
 	Deregister(ctx context.Context, service *ServiceInstance) error
 }
 
+// Discovery 服务发现接口
 type Discovery interface {
-	// GetService return the service instances in memory according to the service name.
+	// 根据服务名称返回内存中的服务实例
 	GetService(ctx context.Context, serviceName string) ([]*ServiceInstance, error)
-	// Watch creates a watcher according to the service name.
+	// 根据服务名称创建观察者
 	Watch(ctx context.Context, serviceName string) (Watcher, error)
+	// 关闭服务发现
 	Close() error
 }
 
+// Watcher  观察者接口
 type Watcher interface {
-	Next() ([]*ServiceInstance, error)
-	// Stop close the watcher.
-	Stopper
+	Next() ([]*ServiceInstance, error) // 获取下一个服务实例
+	Stopper                            // 停止观察者
 }
 
+// Builder 构建器接口
 type Builder interface {
-	Build(id string) Resolver
-	Scheme() string
+	Build(id string) Resolver // 构建解析器
+	Scheme() string           // 获取方案
 }
 
+// Stopper 停止器接口
 type Stopper interface {
-	Close() error
+	Close() error // 关闭停止器
 }
 
+// Resolver 解析器接口
 type Resolver interface {
-	ServiceResolver(ctx context.Context, entries []*consulApi.ServiceEntry) []*ServiceInstance
+	ServiceResolver(ctx context.Context, entries []*consulApi.ServiceEntry) []*ServiceInstance // 服务解析
 }
 
+// 数据中心类型
 const (
-	SingleDataCenter = "SINGLE"
-	MultiDataCenter  = "MULTI"
+	SingleDataCenter = "SINGLE" // 单数据中心
+	MultiDataCenter  = "MULTI"  // 多数据中心
 )
 
+// EntriesOption 条目选项
 type EntriesOption struct {
 	Resolver
 	Service, Tag string
@@ -65,17 +70,8 @@ type EntriesOption struct {
 	Opts         *consulApi.QueryOptions
 }
 
+// Entries 条目接口
 type Entries interface {
-	MultiDCService(ctx context.Context, en *EntriesOption) ([]*ServiceInstance, uint64, error)
-	SingleDCEntries(ctx context.Context, en *EntriesOption) ([]*ServiceInstance, uint64, error)
-}
-
-type Namespace string
-
-const (
-	NamespaceDefault Namespace = "microservices"
-)
-
-type DebugOption struct {
-	IsDebug bool
+	MultiDCService(ctx context.Context, en *EntriesOption) ([]*ServiceInstance, uint64, error)  // 多数据中心服务
+	SingleDCEntries(ctx context.Context, en *EntriesOption) ([]*ServiceInstance, uint64, error) // 单数据中心条目
 }
